91755ebbe603a72382da008977a73b2fab50d291
Chandu-4444 <chandrakiran.g19@iiits.in>
2020-12-18 20:15:08 +0530

    Add CheckBox

---
+++ gui/CheckBox.java
@@ -0,0 +1,67 @@
+package gui;
+
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+
+class CheckBoxFrame extends JFrame
+{
+    private JTextField textField;
+    private JCheckBox boldJCheckBox;
+    private JCheckBox italicJCheckBox;
+
+    public CheckBoxFrame()
+    {
+        super("JCheckBox Test");
+        setLayout(new FlowLayout());
+
+        textField = new JTextField("Watch the font style change", 20);
+        textField.setFont(new Font("Serif", Font.PLAIN, 14));
+        add(textField);
+
+        boldJCheckBox = new JCheckBox("Bold");
+        italicJCheckBox = new JCheckBox("italic");
+        add(boldJCheckBox);
+        add(italicJCheckBox);
+
+        CheckBoxHandler handler = new CheckBoxHandler();
+        boldJCheckBox.addItemListener(handler);
+        italicJCheckBox.addItemListener(handler);
+    }
+
+    private class CheckBoxHandler implements ItemListener
+    {
+        private int valBold = Font.PLAIN;
+        private int valItalic = Font.ITALIC;
+
+        public void itemStateChanged(ItemEvent event)
+        {
+            if(event.getSource() == boldJCheckBox)
+            {
+                valBold = boldJCheckBox.isSelected()? Font.BOLD : Font.PLAIN;
+            }
+            if(event.getSource() == italicJCheckBox)
+            {
+                valItalic = italicJCheckBox.isSelected()?Font.ITALIC : Font.PLAIN;
+            }
+
+            textField.setFont(new
+                    Font("Serif", valBold + valItalic,14));
+        }
+    }
+}
+
+public class CheckBox
+{
+    public static void main(String args[])
+    {
+        CheckBoxFrame frame = new CheckBoxFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(275, 100);
+        frame.setVisible(true);
+    }
+}
